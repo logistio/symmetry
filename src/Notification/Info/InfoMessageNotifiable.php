3fdc15e322e7c64b90664ab5c3a5df2f96213973
Dan <dan.pinzaru93@gmail.com>
2019-01-07 23:37:42 +0000

    Added the slack info notification.

---
+++ src/Notification/Info/InfoMessageNotifiable.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Logistio\Symmetry\Notification\Info;
+
+
+use Illuminate\Notifications\Notifiable;
+use Logistio\Symmetry\Service\Slack\Config\SlackConfig;
+
+class InfoMessageNotifiable
+{
+    use Notifiable;
+
+    /**
+     * @param array $payload
+     */
+    public static function notify(array $payload)
+    {
+        $instance = new self();
+
+        return $instance->sendNotification($payload);
+    }
+
+    /**
+     * @param array $payload
+     */
+    public function sendNotification(array $payload)
+    {
+        $this->notify((new InfoMessageNotification($payload)));
+    }
+
+    public function routeNotificationForSlack()
+    {
+        $config = app()->make(SlackConfig::class);
+        return $config->getWebhookUrl();
+    }
+}
