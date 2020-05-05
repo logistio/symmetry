9536b03b4a9a41281564bc6b798c86b0dbbc2076
Dan Pinzaru <dan.pinzaru93@gmail.com>
2020-05-05 21:51:57 +0100

    Added PostgreUtil class. Added getAllTableNames method.

---
+++ src/Util/Time/Database/PostgresUtil.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Logistio\Symmetry\Util\Time\Database;
+
+use Illuminate\Database\Connection;
+
+/**
+ * Class PostgresUtil
+ * @package Logistio\Symmetry\Util\Time\Database
+ */
+class PostgresUtil
+{
+    /**
+     * @param Connection $connection
+     * @return array
+     */
+    public static function getAllTableNames(Connection $connection)
+    {
+        return $connection->select("
+            SELECT table_name
+            FROM information_schema.tables
+        ");
+    }
+}
